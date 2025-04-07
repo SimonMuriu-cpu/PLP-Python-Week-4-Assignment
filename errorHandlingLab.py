21645065170ab89ac657ab44ac46f9d12de3a222
Simon Muriu <muriusimon6@gmail.com>
2025-04-07 23:37:39 +0300

    Initial commit

---
+++ errorHandlingLab.py
@@ -0,0 +1,13 @@
+# Ask user for file name and handle errors if file does not exist or cannot be read.
+def user_input_file():
+    file_name = input("Enter the file name: ")
+    try:
+        with open(file_name, 'r') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print(f"Error: The file {file_name} does not exist.")
+    except PermissionError:
+        print(f"Error: You do not have permission to read the file {file_name}.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
---
+++ fileReadAndWrite.py.py
@@ -0,0 +1,20 @@
+# A program that reads a file and writes a modified version of it to another file.
+def read_and_write_file(file_name, new_file_name, new_content):
+    try:
+        # reading the content of the original file
+        with open(file_name, 'r') as file:
+            content = file.read()
+     
+     # modifying the content       
+        modified_content = content + new_content
+
+    # writing the modified content to a new file
+        with open(new_file_name, 'w') as new_file:
+            new_file.Write(modified_content)
+
+        print(f"Content from {file_name} has been modified and written to {new_file_name}.")
+    except FileNotFoundError:
+        print(f"Error: The file {file_name} was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
